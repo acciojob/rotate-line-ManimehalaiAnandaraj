ae8f01bb17644e3c1d9437b46db3e946bdcd92f5
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-31 11:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded',()=>{
+	const line=document.getElementById('line');
+	let angle=0;
+
+	function rotateLine() {
+		angle +=2;
+		line.style.transform=`rotate(${angle}deg)`;
+	}
+	setInterval(rotateLine,20)	
+});
